8d1bbb9e609b99ed2d69117c69fbd1ea676e9c3b
David Harrogate <davidharrogate@hotmail.com>
2018-09-15 14:45:04 +0100

    build azure sql

---
+++ VSCODE/create_azure_db.ps1
@@ -0,0 +1,38 @@
+# Login-AzureRmAccount
+# Set the resource group name and location for your server
+$resourcegroupname = "myResourceGroupSQL"
+$location = "UK West"
+# Set an admin login and password for your server
+$adminlogin = "ServerAdmin"
+$password = "!Qazxsw23edc"
+# Set server name - the logical server name has to be unique in the system
+$servername = "server-$(Get-Random)"
+# The sample database name
+$databasename = "mySampleDatabase"
+# The ip address range that you want to allow to access your server
+$startip = "10.0.1.1"
+$endip = "10.0.1.10"
+
+# Create a resource group
+$resourcegroup = New-AzureRmResourceGroup -Name $resourcegroupname -Location $location
+
+# Create a server with a system wide unique server name
+$server = New-AzureRmSqlServer -ResourceGroupName $resourcegroupname `
+    -ServerName $servername `
+    -Location $location `
+    -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminlogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+# Create a server firewall rule that allows access from the specified IP range
+$serverfirewallrule = New-AzureRmSqlServerFirewallRule -ResourceGroupName $resourcegroupname `
+    -ServerName $servername `
+    -FirewallRuleName "AllowedIPs" -StartIpAddress $startip -EndIpAddress $endip
+
+# Create a blank database with an S0 performance level
+$database = New-AzureRmSqlDatabase  -ResourceGroupName $resourcegroupname `
+    -ServerName $servername `
+    -DatabaseName $databasename `
+    -RequestedServiceObjectiveName "S0" `
+    -SampleName "AdventureWorksLT"
+
+# Clean up deployment 
+# Remove-AzureRmResourceGroup -ResourceGroupName $resourcegroupname
